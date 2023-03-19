b47fb13d0a0081fa5626d1ac6cdc341d749b8cc6
Bl4ckSlayer <amirulislamtajbid@gmail.com>
2023-03-19 13:36:53 +0600

    fixed

---
+++ src/helpers/hitToast.js
@@ -0,0 +1,41 @@
+import { toast } from "react-toastify";
+const hitToast = (message, variant) => {
+  // if (!["success", "error"].includes(message)) {
+  //   toast(`${message}`, {
+  //     type: `${variant}`,
+  //     position: "top-right",
+  //     autoClose: 3000,
+  //     hideProgressBar: false,
+  //     closeOnClick: true,
+  //     pauseOnHover: true,
+  //     draggable: true,
+  //     progress: undefined,
+  //   });
+  //   return;
+  // }
+  toast(`${message}`, {
+    type: `${variant}`,
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+export default hitToast;
+// import { toast } from "react-toastify";
+// const hitToast = (variant, message) => {
+//   if (!["success", "error"].includes(variant)) throw Error();
+//   toast[`${variant}`](message, {
+//     position: "top-right",
+//     autoClose: 3000,
+//     hideProgressBar: false,
+//     closeOnClick: true,
+//     pauseOnHover: true,
+//     draggable: true,
+//     progress: undefined,
+//   });
+// };
+// export default hitToast;
